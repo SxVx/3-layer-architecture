68206437fa84052e33d98fc4b5f988b11dae1315
José Enrique <drilosv13@gmail.com>
2023-10-13 23:59:21 -0600

    fix: resolved writting in example db.json

--- src/databse/utils.js
+++ src/databse/utils.js
@@ -4,19 +4,11 @@ const saveDataBase = (db) => {
   const data = JSON.stringify(db, null, 2);
   
   try {
-    console.log(data);
-    fs.writeFileSync('./db.json', data, { encoding: 'utf8', flags: 'w' });
+    fs.writeFileSync('./src/database/db.json', data, { encoding: 'utf8', flags: 'w' });
     console.log('Archivo guardado con éxito.');
   } catch (error) {
     console.error('Error al guardar el archivo:', error);
   }
-  // fs.writeFile('./db.json', data, (error) => {
-  //   if (error) {
-  //     console.error('Error al escribir en el archivo:', error);
-  //   } else {
-  //     console.log('Se ha escrito en el archivo satisfactoriamente.');
-  //   }
-  // });
 }
 
 
